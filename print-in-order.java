9e587800a847c0775aed40e98524c1a96e97c083
MessyOrnate <53431182+MessyOrnate@users.noreply.github.com>
2019-12-06 16:57:34 +0800

    new

--- print-in-order.java
+++ print-in-order.java
@@ -1,22 +1,28 @@
 class Foo {
+    private volatile int count;
 
     public Foo() {
-        
+
     }
-    volatile int count=1;
+
     public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
         count++;
+
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while (count!=2);
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        while(count != 1) { }
         printSecond.run();
         count++;
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while (count!=3);
+        // printThird.run() outputs "third". Do not change or remove this line.
+        while(count != 2) { }
         printThird.run();
     }
 }
+
